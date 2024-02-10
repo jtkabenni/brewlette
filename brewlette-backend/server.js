b9d719a761b2aafdeb5be2205a3fe6a36b44806c
Billy Kaplan <billy1kaplan@gmail.com>
2024-02-09 20:53:19 -0800

    Delete Everything

--- brewlette-backend/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const app = require("./app");
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, function () {
-  console.log(`Started on http://localhost:${PORT}`);
-});
--- brewlette-frontend/src/components/Logo.js
+++
@@ -1,5 +0,0 @@
-import logo from "../logo.png";
-
-export default function Logo() {
-  return <img src={logo} style={{ width: "200px" }} />;
-}
